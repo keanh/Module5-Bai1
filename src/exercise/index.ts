0c1e100e34f3bb4566316691093015ace80fab63
keanh <keanh275@gmail.com>
2020-06-02 17:45:07 +0700

    add part 2

--- src/exercise/index.ts
+++ src/exercise/index.ts
@@ -6,7 +6,8 @@ interface IRepos {
   items: Array<ISingleRepo>;
 }
 async function fetchRepo(): Promise<Array<ISingleRepo>> {
-  let res: Response | IRepos = await fetch('https://api.github.com/search/repositories?q=angular');
+  // Sử dụng hàm fetch để gửi request và nhận về dữ liệu
+  let res: Response | IRepos = await fetch('https://api.github.com/search/repositories?q=facebook');
   res = await res.json() as IRepos;
   return res.items;
 }
@@ -17,11 +18,37 @@ function createItem(text: string): HTMLLIElement {
   return item;
 }
 
+async function getUser(value: string): Promise<Array<ISingleRepo>> {
+  try {
+    let res: Response | IRepos = await fetch(`https://api.github.com/search/repositories?q=${value}`);
+    res = await res.json() as IRepos;
+    return res.items;
+  } catch (e) {
+    throw e;
+  }
+}
+
+// async function test(): Promise<Array<ISingleRepo>> {
+//   try {
+//     let value = "facebook";
+//     let res: Response | IRepos = await fetch(`https://api.github.com/search/repositories?q=${value}`);
+//     res = await res.json() as IRepos;
+//     return res.items;
+//   } catch (e) {
+//     throw e;
+//   }
+// }
+
 const container = document.querySelector('.app .list');
 
 async function main() {
   // step 1: fetch repo
-  const res = await fetchRepo();
+  // const res = await fetchRepo();
+  const queryString = window.location.search;
+  let searchParam = new URLSearchParams(queryString);
+  let value = searchParam.get('search');
+  const res = await getUser(value);
+  // const res = await test();
   // step 2: lặp qua mảng các item trả về
   // step 3: call hàm createItem sau đó truyền vào name của từng item ở mỗi vòng lặp
   // step 4: call hàm container.appendChild(item mà hàm createItem trả về)
